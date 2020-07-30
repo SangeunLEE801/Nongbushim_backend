acf660a48a8d8448e0da77b4b9050db213e588e0
scarlett.lee.git <scarlett.lee.git@gmail.com>
2020-07-30 21:12:37 +0900

    Rename controller

--- src/main/java/com/nongbushim/SearchController.java
+++ src/main/java/com/nongbushim/SearchController.java
@@ -7,16 +7,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-public class Controller {
+public class SearchController {
 
     @GetMapping("/search")
     public String callApi() throws JsonProcessingException {
@@ -47,4 +51,30 @@ public class Controller {
         return jsonInString;
     }
 
+    @GetMapping("/nameAutoComplete")
+    @ResponseBody
+    public List<String> nameAutoComplete(@RequestParam(value = "input", required=false, defaultValue = "") String input) {
+        List<String> suggestions = new ArrayList<>();
+        suggestions.add("쌀 일반계 상품");
+        suggestions.add("쌀 일반계 중품");
+        suggestions.add("피마늘 한지1접 상품");
+        suggestions.add("피마늘 한지1접 중품");
+        suggestions.add("피마늘 난지1접 상품");
+        suggestions.add("피마늘 난지1접 중품");
+        suggestions.add("피마늘 한지 상품");
+        suggestions.add("피마늘 한지 중품");
+        suggestions.add("피마늘 난지 상품");
+        suggestions.add("피마늘 난지 중품");
+        suggestions.add("피마늘 햇한지1접 상품");
+        suggestions.add("피마늘 햇한지1접 중품");
+        suggestions.add("피마늘 햇난지1접 상품");
+        suggestions.add("피마늘 햇난지1접 중품");
+        suggestions.add("피마늘 쪽마늘 상품");
+        suggestions.add("피마늘 쪽마늘 중품");
+        suggestions.add("피마늘 햇한지 상품");
+        suggestions.add("피마늘 햇한지 중품");
+        suggestions.add("피마늘 햇난지 상품");
+        suggestions.add("피마늘 햇난지 중품");
+        return suggestions;
+    }
 }
